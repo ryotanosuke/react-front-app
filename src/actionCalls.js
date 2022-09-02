2f86f29a43a292d2b36ea12be0488961f4e7b470
ryotanosuke <sub_player-github@yahoo.co.jp>
2022-09-02 11:47:26 +0900

    auth imp

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -3,9 +3,10 @@ import axios from "axios";
 // login.jsから呼び出され,userにはアドレスが渡る
 export const loginCall = async (user, dispatch, proxy) => {
   dispatch({ type: "LOGIN_START" });
+  const prox = "https://real-sns-nodejs.herokuapp.com/api";
   try {
     // サーバーにアドレス(user)を送って一致するオブジェクトを返す
-    const response = await axios.post(`${proxy}auth/login`, user);
+    const response = await axios.post(`${prox}auth/login`, user);
 
     // dispatchでuserに対して取得したオブジェクト(payload)を格納する
     // type によって取得した payload をどこに格納するか決定する
